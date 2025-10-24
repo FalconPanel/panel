7cbaab7ea64836155115d914b44e69259f5f7122
Falcon <brynleyportillo0@gmail.com>
2025-10-23 21:08:40 -0500

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content-header')
-    <h1>Administrative Overview<small>A quick glance at your system.</small></h1>
+    <h1>Administrative Overview<small>A quick look at your Falcon system.</small></h1>
     <ol class="breadcrumb">
         <li><a href="{{ route('admin.index') }}">Admin</a></li>
         <li class="active">Index</li>
@@ -27,27 +27,40 @@
             </div>
             <div class="box-body">
                 @if ($version->isLatestPanel())
-                    You are running Pterodactyl Panel version <code>{{ config('app.version') }}</code>. Your panel is up-to-date!
+                    You are running Falcon Panel version <code>{{ config('app.version') }}</code>. Your panel is up-to-date!
                 @else
-                    Your panel is <strong>not up-to-date!</strong> The latest version is <a href="https://github.com/Pterodactyl/Panel/releases/v{{ $version->getPanel() }}" target="_blank"><code>{{ $version->getPanel() }}</code></a> and you are currently running version <code>{{ config('app.version') }}</code>.
+                    Your panel is <strong>not up-to-date!</strong> The latest version is 
+                    <a href="https://github.com/FalconPanel/panel/releases/v{{ $version->getPanel() }}" target="_blank">
+                        <code>{{ $version->getPanel() }}</code>
+                    </a> 
+                    and you are currently running version <code>{{ config('app.version') }}</code>.
                 @endif
             </div>
         </div>
     </div>
 </div>
 <div class="row">
-    <div class="col-xs-6 col-sm-3 text-center">
-        <a href="{{ $version->getDiscord() }}"><button class="btn btn-warning" style="width:100%;"><i class="fa fa-fw fa-support"></i> Get Help <small>(via Discord)</small></button></a>
+    <div class="col-xs-6 col-sm-4 text-center">
+        <a href="{{ $version->getDiscord() }}">
+            <button class="btn btn-warning" style="width:100%;">
+                <i class="fa fa-fw fa-support"></i> Get Help <small>(via Discord)</small>
+            </button>
+        </a>
     </div>
-    <div class="col-xs-6 col-sm-3 text-center">
-        <a href="https://pterodactyl.io"><button class="btn btn-primary" style="width:100%;"><i class="fa fa-fw fa-link"></i> Documentation</button></a>
+    <div class="col-xs-6 col-sm-4 text-center">
+        <a href="https://falcon.netlify.app/docs">
+            <button class="btn btn-primary" style="width:100%;">
+                <i class="fa fa-fw fa-link"></i> Documentation
+            </button>
+        </a>
     </div>
     <div class="clearfix visible-xs-block">&nbsp;</div>
-    <div class="col-xs-6 col-sm-3 text-center">
-        <a href="https://github.com/pterodactyl/panel"><button class="btn btn-primary" style="width:100%;"><i class="fa fa-fw fa-support"></i> GitHub</button></a>
-    </div>
-    <div class="col-xs-6 col-sm-3 text-center">
-        <a href="{{ $version->getDonations() }}"><button class="btn btn-success" style="width:100%;"><i class="fa fa-fw fa-money"></i> Support the Project</button></a>
+    <div class="col-xs-6 col-sm-4 text-center">
+        <a href="https://github.com/FalconPanel/panel">
+            <button class="btn btn-primary" style="width:100%;">
+                <i class="fa fa-fw fa-github"></i> GitHub
+            </button>
+        </a>
     </div>
 </div>
 @endsection
